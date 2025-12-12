27e05561a54163200577d14f06c738f77c73fdb7
ilyasaitbel <ilyasilya084@gmail.com>
2025-12-12 16:23:22 +0100

    ajouter sql & use case & diagrame de class

---
+++ sql & use case & diagrame de class/shema.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE digital_garden;
+
+USE digital_garden;
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(50) UNIQUE,
+  password_hash VARCHAR(255),
+  date_created DATE
+);
+
+CREATE TABLE themes (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT,
+  name VARCHAR(100),
+  color VARCHAR(20),
+  tags VARCHAR(255),
+  date_created DATE,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE notes (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT,
+  theme_id INT,
+  title VARCHAR(150),
+  importance INT,
+  content TEXT,
+  date_created DATE,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (theme_id) REFERENCES themes(id)
+);
